0334c59ebdafd28a5735a47c9bfdb124730dde6e
sumonDev13 <sumonmondal1530765@gmail.com>
2023-12-28 20:35:21 +0600

    initial commit

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,15 @@
+import Hero from "../../components/heroSection/Hero";
+import Navbar from "../../components/navbar/Navbar";
+import Topnavbar from "../../components/navbar/Topnavbar";
+
+const Home = () => {
+  return (
+    <div>
+      <Topnavbar/>
+      <Navbar/>
+      <Hero/>
+    </div>
+  );
+};
+
+export default Home;
